c8f75340288746b3cf987e547b67752c60f11a28
Sam Birch <sam.m.birch@gmail.com>
2014-02-28 21:04:23 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hold',
+      version='0.1',
+      description='A small Python module for incremental computation',
+      url='https://github.com/sbirch/hold',
+      author='Sam Birch',
+      author_email='sam.m.birch@gmail.com',
+      license='MIT',
+      packages=['hold'],
+      zip_safe=False)
